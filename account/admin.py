c9e2fed08ad09a711857d7a775e3d152b582f6dc
unknown <mohit_creed@yahoo.com>
2019-03-28 15:48:14 +0530

    initial commit

---
+++ account/admin.py
@@ -0,0 +1,26 @@
+from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin as DjangoUserAdmin
+from django.utils.translation import ugettext_lazy as _
+
+from .models import User
+# Register your models here.
+@admin.register(User)
+class UserAdmin(DjangoUserAdmin):
+    """ Define admin model for custom User model with no username field """
+
+    fieldsets = (
+        (None, {'fields':('email', 'password')}),
+        (_('Personal Info'), {'fields':('first_name','last_name')}),
+        (_('Permissions'), {'fields':('is_active','is_staff', 'is_superuser','groups', 'user_permissions')}),
+    )
+
+    add_fieldsets = (
+        (None, {
+            'classes':('wide',),
+            'fields':('email', 'password1', 'password2'),
+            }),
+    )
+
+    list_display = ('email', 'first_name', 'last_name', 'is_staff')
+    search_fields = ('email','first_name', 'last_name')
+    ordering = ('email',)
